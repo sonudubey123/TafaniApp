a651355cc0040567242d583b889a9686b8877348
User <sharique9718@gmail.com>
2022-09-27 20:27:05 +0530

    newapp added nn check

--- app/src/main/java/com/sunmi/tafani_printerhelper/nextgen_sharique/account_balance/AccountBalanceRetailer.java
+++ app/src/main/java/com/sunmi/tafani_printerhelper/nextgen_sharique/account_balance/AccountBalanceRetailer.java
@@ -46,6 +46,7 @@ public class AccountBalanceRetailer extends AppCompatActivity implements View.On
 
     private boolean isBold, isUnderLine;
     private String testFont;
+    //s
 
 
     String amountFromServer="",transactionStatus="",transid="",transactionDate="",transactionTime="";
