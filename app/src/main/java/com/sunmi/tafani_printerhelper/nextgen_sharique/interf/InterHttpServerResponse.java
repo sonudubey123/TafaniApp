b5e5b0fe59c77d244f7199d4ad08f5ff07124e22
User <sharique9718@gmail.com>
2022-10-06 15:19:48 +0530

    bugs fix

--- app/src/main/java/com/sunmi/tafani_printerhelper/nextgen_sharique/interf/InterHttpServerResponse.java
+++
@@ -1,8 +0,0 @@
-package com.sunmi.tafani_printerhelper.nextgen_sharique.interf;
-
-import org.json.JSONObject;
-
-public interface InterHttpServerResponse {
-
-    public void serverResponse(int requestNo, JSONObject serverResponse);
-}
